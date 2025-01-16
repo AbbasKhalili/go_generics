00732a0fbdde8b8a42ef73599ff5ecaea062688a
Abbas Khalili <khalilikhoo2@hotmail.com>
2025-01-16 13:52:19 +0330

    calc map and show on screen

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
 
+	intDic := map[string]int64{
+		"sunday":   100,
+		"tuesday":  300,
+		"thursday": 500,
+	}
+
+	floatDic := map[string]float64{
+		"saturday":  12.5,
+		"monday":    14.5,
+		"wednesday": 16.5,
+	}
+
+	var intVal, intTitle = SumInts(&intDic)
+	var floatVal, floatTitle = SumFloats(&floatDic)
+
+	fmt.Println(intDic, "sum int :", intVal, intTitle)
+	fmt.Println(floatDic, "sum float :", floatVal, floatTitle)
 }
 
-func SumInts(dic map[string]int64) int64 {
+func SumInts(dic *map[string]int64) (int64, string) {
 	var num int64
-	for _, val := range dic {
+	var title strings.Builder
+	for key, val := range *dic {
 		num += val
+		title.WriteString(key + "|")
 	}
-	return num
+	return num, title.String()
 }
 
-func SumFloats(dic map[string]float64) float64 {
+func SumFloats(dic *map[string]float64) (float64, string) {
 	var num float64
-	for _, val := range dic {
+	var title strings.Builder
+	for key, val := range *dic {
 		num += val
+		title.WriteString(key + "|")
 	}
-	return num
+	return num, title.String()
 }
